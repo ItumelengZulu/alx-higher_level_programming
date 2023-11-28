06a5bfb9cc6af9481d5e84da30aba2927e35ced3
ItumelengZulu <itumelengzulu@gmail.com>
2023-11-28 02:43:48 +0200

    not the normal Python - Hello, World

---
+++ 0x00-python-hello_world/2-print.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# this python script feels exactlty like a piece in a puzzle
+print("Programming is like building a multilingual puzzle")
+
+#somehow feels like it wont work smh, yes i didnt u 
+#you cant write python code with a shebang #!/bin/bash
+#uses(#!/usr/bin/python3) for python instead
+
