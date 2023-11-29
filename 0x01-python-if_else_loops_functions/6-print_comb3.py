8eeead58995a1c3da4a05f2410257a25d6060ce8
ItumelengZulu <itumelengzulu@gmail.com>
2023-11-30 01:13:43 +0200

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 
 for first_number in range(1, 10):
     for second_number in range(first_number + 1, 10):
-        print("{:02}, {:02}".format(first_number, second_number), end=", ")
+        print("{:02}, {:02}".format(first_number, second_number), end=" ")
 print()
