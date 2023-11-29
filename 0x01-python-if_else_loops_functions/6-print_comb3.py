634fcba65d32097fc674240cd383c9d69f3c855d
ItumelengZulu <itumelengzulu@gmail.com>
2023-11-30 01:08:36 +0200

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,9 +2,6 @@
 
 for first_digit in range(10):
     for second_digit in range(first_digit + 1, 10):
-        print(
-            "{:02}".format(first_digit), end="") 
-        if second_digit < 9:
-            print(", ",end="")
-        else:
-            print()
+        print("{:02}".format(first_digit), end="")
+        print("{:02}".format(second_digit), end='')
+print()
