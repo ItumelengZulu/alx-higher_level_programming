0e815cb73f38930fd8fee55c43b0e3fb8f412340
ItumelengZulu <itumelengzulu@gmail.com>
2023-11-29 05:46:03 +0200

    Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+def uppercase(s):
+    for char in s:
+        uppercase_char = char
+        if 'a' <= char <= 'z':
+            uppercase_char = chr(ord(char) - ord('a') + ord('A'))
+        print("{}".format(uppercase_char), end='')
+    print()
